ae8b527a4cce8d4121bc9cc526859fa97ae216a9
JeronimoVasquez <jeronimovasquez1307@gmail.com>
2023-08-21 16:46:21 -0500

    Se crea la clase Auto, sus atribuso y métodos

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,49 @@
+package test;
+
+public class Auto {
+	
+	public String modelo;
+	public int precio;
+	public Asiento[] asientos;
+	public String marca;
+	public Motor motor;
+	public int registro;
+	public static int cantidadCreados;
+	
+	
+	public int cantidadAsientos() {
+		
+		return asientos.length;
+		
+	}
+	
+	public String verificarintegridad() {
+		if(this.registro == this.motor.registro) {
+			
+			int c = 0;
+			
+			for(int i = 0;i > asientos.length; i++) {
+				
+				if (asientos[i].registro == this.registro)
+					c++;
+				
+				
+			}
+			
+			if (c == asientos.length) {
+				
+				return "Auto original";
+				
+			}
+			else {
+				return "Las piezas no son originales";
+			}
+		}
+		else {
+			return "Las piezas no son originales";
+			
+		}
+	}
+ 	
+
+}
---
+++ src/test/java/test/Motor.java
@@ -0,0 +1,30 @@
+package test;
+
+public class Motor {
+	
+	public int numeroCilindros;
+	public String tipo;
+	public int registro;
+	
+	
+	public void cambiarRegistro(int registro) {
+		
+		this.registro = registro;
+	}
+	
+	public void asignarTipo(String tipo) {
+		
+		if (tipo == "electrico")
+			this.tipo =  tipo;
+		else if (tipo == "gasolina")
+			this.tipo = tipo;
+		else {
+			System.out.println("Tipo invalido");
+		}
+		
+	}
+	
+	
+	
+
+}
